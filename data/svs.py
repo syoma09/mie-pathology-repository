c3ad5dd6a198c72a2b6f0eaee513ee0621ff6940
morita <morita@hi.info.mie-u.ac.jp>
2020-12-23 17:53:20 +0900

    Write patch extractor

--- data/svs.py
+++ data/svs.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 
 import numpy as np
+from PIL import Image, ImageDraw
 from openslide import OpenSlide
 
 
@@ -28,6 +29,21 @@ class SVS(object):
         print("SVS() load annotation from:", annotation)
         self.annotation = SVSAnnotation(annotation)
 
+        self.mask = self.__create_mask(
+            self.image.slide.dimensions,
+            self.annotation.Vertices
+        )
+
+    @staticmethod
+    def __create_mask(shape, polygon):
+        mask = Image.new("1", shape, 0)
+        ImageDraw.Draw(mask).polygon(polygon, fill=1)
+
+        # WARNING: This conversion requires much memory
+        # mask = np.array(mask, dtype=bool)
+
+        return mask
+
     def get_thumbnail(self, shape):
         dims = self.image.slide.dimensions
         zoom = min(shape[0] / dims[0], shape[1] / dims[1])
@@ -39,15 +55,37 @@ class SVS(object):
 
         return image, annot
 
+    def patches(self, size=(256, 256), stride=(64, 64)):
+        shape = self.image.slide.dimensions
+
+        for j in range(0, shape[1], stride[1]):
+            for i in range(0, shape[0], stride[0]):
+                yield (i, j), (i + size[0], j + size[1])
+
+    def extract_img_mask(self, location, size):
+        img = np.array(
+            self.image.slide.read_region(location, 0, size)
+        )
+
+        mask = self.mask.crop(box=(
+            location[0],
+            location[1],
+            location[0] + size[0],
+            location[1] + size[1]
+        ))
+        mask = np.array(mask, dtype='uint8') * 255
+
+        return img, mask
+
 
 class SVSImage(object):
     def __init__(self, path):
         self.slide = OpenSlide(str(path))
 
         # print("level_count: ", self.slide.level_count)
-        print("dimensions : ", self.slide.dimensions)
-        # img = self.slide.get_thumbnail((512, 512))
-        # img = slide.get_thumbnail(slide.dimensions)
+        # print("dimensions : ", self.slide.dimensions)
+
+        # img = self.slide.get_thumbnail(self.slide.dimensions)
         # print(img)
         # img.save("./test.jpg")
 
