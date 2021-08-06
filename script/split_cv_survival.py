12fc69866b08f306f36877845fcb15b8679c9bfc
morita <morita@hi.info.mie-u.ac.jp>
2021-08-06 13:53:24 +0900

    Create auto cv splitter

--- script/split_cv_survival.py
+++ script/split_cv_survival.py
@@ -3,6 +3,7 @@
 
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 
@@ -35,40 +36,76 @@ def split_cv(counts, cv=4):
     counts = sorted(counts, key=lambda x: x[1], reverse=True)
     print(counts)
 
-    # Reverse order of even chunks
-    def chunk_rev_range(i):
-        if i // cv % 2 == 0:
-            return range(i, i + cv)
-        else:
-            return range(i + cv - 1, i - 1, -1)
-    counts = [
-        counts[ci]
-        for i in range(0, len(counts), cv)
-        for ci in chunk_rev_range(i)
-        if ci < len(counts)
-    ]
-    print(counts)
+    # # Reverse order of even chunks
+    # def chunk_rev_range(i):
+    #     if i // cv % 2 == 0:
+    #         return range(i, i + cv)
+    #     else:
+    #         return range(i + cv - 1, i - 1, -1)
+    # counts = [
+    #     counts[ci]
+    #     for i in range(0, len(counts), cv)
+    #     for ci in chunk_rev_range(i)
+    #     if ci < len(counts)
+    # ]
+    # print(counts)
 
-    cv_dataset = [
-        counts[i::cv]
-        for i in range(cv)
-    ]
-    for d in cv_dataset:
+    cv_dataset = [[[], 0] for _ in range(cv)]
+    for data in counts:
+        idx = np.argmin([n for [_, n] in cv_dataset])
+
+        cv_dataset[idx][0].append(data)
+        cv_dataset[idx][1] += data[1]
+
+    for [d, _] in cv_dataset:
         print(d, sum(n for _, n in d))
-    print(cv_dataset)
+
+    return [
+        dataset for [dataset, _] in cv_dataset
+    ]
 
 
 def main():
-    df = pd.read_csv(Path(
+    # Source annotation file
+    src = Path(
         "~/workspace/mie-pathology/_data/survival_cls.csv"
-    ).expanduser())
+    ).expanduser()
+    # Destination root
+    dst = src.parent / src.stem
+    dst.mkdir(parents=True, exist_ok=True)
 
-    # print(df)
+    # Load annotation
+    df = pd.read_csv(src)
 
     # Retrieve non-survivers
     df_neg = df[df.label == 0]
-    # print(df_neg)
-    split_cv(count_subject_image(df_neg))
+    cv_neg = split_cv(count_subject_image(df_neg))
+
+    # Retrieve survivers
+    df_pos = df[df.label == 1]
+    cv_pos = split_cv(count_subject_image(df_pos))
+
+    # Sort
+    def sort_key(dataset):
+        return sum(
+            n for _, n in dataset
+        )
+    cv_neg = sorted(cv_neg, key=sort_key)
+    cv_pos = sorted(cv_pos, key=sort_key, reverse=True)
+
+    for cv, (valid_neg, valid_pos) in enumerate(zip(cv_neg, cv_pos)):
+        # Set all data as training (tvt==0)
+        df['tvt'] = 0
+        for subject, _ in valid_neg + valid_pos:
+            df.loc[
+                df['number'].str.startswith(subject), 'tvt'
+            ] = 1
+
+        print("##################################################")
+        print(df)
+        df.to_csv(
+            dst / f"cv{cv}.csv", index=False
+        )
 
 
 if __name__ == '__main__':
