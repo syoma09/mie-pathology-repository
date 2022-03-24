9999df46b024d141a1cf5525397d10046e490f1c
morita <morita@hi.info.mie-u.ac.jp>
2022-03-24 13:35:36 +0900

    Add script to chek WSI dimension

---
+++ script/check_dimension.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+import pandas as pd
+
+from data.svs import SVS
+
+
+def main():
+    src = Path("~/workspace/mie-pathology/_data/").expanduser()
+
+    df = pd.read_csv(src / "survival_2dfs_v2.csv")
+    print(df)
+
+    for _, row in df.iterrows():
+        subject = row['number']
+
+        svs = SVS(
+            src / "svs" / f"{subject}.svs",
+            src / "xml" / f"{subject}.xml"
+        )
+
+        print(f"Subject-{subject}: {svs.image.slide.dimensions}")
+
+
+if __name__ == '__main__':
+    main()
