3a7975660d9a640052f43e1e4350d43fa10d32b3
Yasuhude-Nonaka <418840@m.mie-u.ac.jp>
2022-04-05 19:46:40 +0900

    add survival_time directory

--- script/check_dimension.py
+++ script/check_dimension.py
@@ -3,7 +3,7 @@
 
 from pathlib import Path
 
-import pandas as pd=
+import pandas as pd
 
 from data.svs import SVS
 
--- survival_time/plot.py
+++ survival_time/plot.py
@@ -10,16 +10,13 @@ import torch.nn as nn
 import torch.utils.data
 import torchvision
 import yaml
-import numpy as np
-import matplotlib
-matplotlib.use('Agg') # -----(1)
+import numpy
 import matplotlib.pyplot as plt
-import AutoEncoder
 from torch.backends import cudnn
 from PIL import Image
 from PIL import ImageFile
+
 from cnn.metrics import ConfusionMatrix
-from scipy.stats import norm
 
 
 device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
@@ -34,13 +31,13 @@ class PatchDataset(torch.utils.data.Dataset):
         self.transform = torchvision.transforms.Compose([
             # torchvision.transforms.Resize((224, 224)),
             torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
+            # torchvision.transforms.Normalize(0.5, 0.5)
         ])
 
         self.paths = []
         for subject in subjects:
             self.paths += list((root / subject).iterdir())
-        
+
         # print(self.paths[0])
         # print(len(self.paths))
 
@@ -62,13 +59,8 @@ class PatchDataset(torch.utils.data.Dataset):
         name = self.paths[item].name                # Filename
         label = float(str(name).split('_')[1][2:])  # Survival time
 
-        # Classsification
-        '''if(label < 10):
-            label = 0
-        elif(label < 31):
-            label = 1
-        elif(label < 67):
-            label = 2'''
+        # Normalize
+        label /= 90.
         # Tensor
         label = torch.tensor(label, dtype=torch.float)
 
@@ -83,27 +75,31 @@ class PatchDataset(torch.utils.data.Dataset):
     #print("      recall      : {:3.3}".format(metrics['cmat'].tnr))
     #print("      Matrix:")
     #print(metrics['cmat'])
-class AutoEncoder2(torch.nn.Module):
-     def __init__(self, enc, dec):
-         super().__init__()
-         self.enc = enc
-         self.dec = dec
-     def forward(self, x):
-         x = self.enc(x)
-         x = self.dec(x)
-         return x
 
-def imshow(img):
-    img = torchvision.utils.make_grid(img)
-    img = img / 2 + 0.5
-    npimg = img.detach().numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-    
+def create_model():
+
+# model = torchvision.models.resnet152(pretrained=False)
+    # model = torchvision.models.resnet152(pretrained=True)
+    net = torchvision.models.resnet50(pretrained=True)
+    # print(model)
+
+    # Replace FC layer
+    num_features = net.fc.in_features
+    # print(num_features)  # 512
+    net.fc = nn.Sequential(
+        nn.Linear(num_features, 1, bias=True),
+        # nn.Softmax(dim=1)
+        # nn.Sigmoid()
+    )
+    net = net.to(device)
+    # net = torch.nn.DataParallel(net).to(device)
+
+    return net
+
 def main():
     src = Path("~/root/workspace/mie-pathology/_data/").expanduser()
     #root = Path("~/data/_out/mie-pathology/").expanduser()
-    dataset_root = Path('/mnt/cache') / os.environ.get('USER') / 'mie-pathology'
+    dataset_root = Path("~/root/mnt/cache").expanduser() / os.environ.get('USER'                                                                                                                                    ) / 'mie-pathology'
     batch_size = 32
     if not dataset_root.exists():
         dataset_root.mkdir(parents=True,exist_ok=True)
@@ -137,59 +133,36 @@ def main():
 
     # データ読み込み
     train_loader = torch.utils.data.DataLoader(
-     PatchDataset(dataset_root, yml['train']), batch_size=batch_size,
+        PatchDataset(dataset_root, yml['train']), batch_size=batch_size, shuffle                                                                                                                                    =True,
         num_workers=num_workers
     )
-    '''valid_loader = torch.utils.data.DataLoader(
-     PatchDataset(dataset_root, yml['valid']), batch_size=batch_size, shuffle=True,
+    valid_loader = torch.utils.data.DataLoader(
+     PatchDataset(dataset_root, yml['valid']), batch_size=batch_size,
         num_workers=num_workers
-    )'''
-    net = AutoEncoder.create_model().to(device)
+    )
+    net = create_model().to(device)
     net.load_state_dict(torch.load(
-    dataset_root/ '20220321_165400model00269.pth', map_location=device))
-    output_and_label = []
-    #print(net)
-    for batch, (x, y_true) in enumerate(train_loader):
-            x, y_true = x.to(device), y_true.to(device)
-            y_pred = net(x) 
-            print("\r  Batch({:6}/{:6})[{}]: ".format(
-                batch, len(train_loader),
-                ('=' * (30 * batch // len(train_loader)) + " " * 30)[:30]
-            ), end="")
-    output_and_label.append((y_pred, x))
-    org, img = output_and_label[-1]
-    img = img[0:1]
-    org = org[0:1]
-    plt.figure(figsize=(1, 1))
-    imshow(org.cpu())
-    ax = plt.gca()
-    ax.axes.xaxis.set_visible(False)
-    ax.axes.yaxis.set_visible(False)
-    plt.savefig('figure1.png')
-    plt.figure(figsize=(1, 1))
-    imshow(img.cpu())
-    ax = plt.gca()
-    ax.axes.xaxis.set_visible(False)
-    ax.axes.yaxis.set_visible(False)
-    plt.savefig('figure2.png') # -----(2)
-    '''for batch, (x, y_true) in enumerate(valid_loader):
-        x, y_true = x.to(device), y_true.to(device)
-        y_pred = net(x)
-        print("\r  Batch({:6}/{:6})[{}]: ".format(
-                batch, len(valid_loader),
-                 ('=' * (30 * batch // len(valid_loader)) + " " * 30)[:30]
-             ), end="")
-    output_and_label.append((y_pred, x))
-    org, img = output_and_label[-1]
-    img = img[0:3]
-    org = org[0:3]
-
-    plt.figure(figsize=(3, 3))
-    imshow(org.cpu())
-    plt.savefig('figure7.png')
-    plt.figure(figsize=(3, 3))
-    imshow(img.cpu())
-    plt.savefig('figure8.png') # -----(2)'''
+    dataset_root / '20210706_233358model00000.pth', map_location=device))
+    fig ,ax = plt.subplots()
+    d_today = datetime.date.today()
+    with torch.no_grad():
+        net.eval()
+        y_true_plot = []
+        y_pred_plot = []
+        j = 0
+        for batch,(x, y_true) in enumerate(valid_loader):
+            j += 1
+            print(j)
+            x,y_true = x.to(device), y_true.to(device)
+            y_pred = net(x)   # Forward
+            y_true_plot.append(y_true.cpu().numpy().tolist())
+            y_pred_plot.append(y_pred.cpu().numpy().flatten().tolist())
+        i = 1
+        for i in range(len(y_true_plot)):
+            print(i)
+            ax.plot(y_true_plot[i],y_pred_plot[i],'.')
+            fig.savefig(f'valid{d_today}00000.png')
+        print(f'valid00000.png')
 
 if __name__ == '__main__':
-     main()
+    main()
